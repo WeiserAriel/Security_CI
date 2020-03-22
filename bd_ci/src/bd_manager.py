0eb57257fac87a5577d6ae6497a46e343fdd137e
WeiserAriel <41187619+WeiserAriel@users.noreply.github.com>
2020-03-22 17:44:09 +0200

    Update bd_manager.py

--- bd_ci/src/bd_manager.py
+++ bd_ci/src/bd_manager.py
@@ -98,7 +98,7 @@ def clone_repo(project, url):
             for directory in('common_mlnx','demo','generic','switchx'):                     
                 dir_ = os.path.abspath(os.getcwd()) + '/tree/customer/' + directory          
                 print('removing directory : ' + str(dir_))                                  
-                os.rmtree(dir_)  
+                shutil.rmtree(dir_)  
             print('running git checkout for master')
             os.system('git checkout ufmapl_4_2_0_ufm_6_3_0')
             print('git checkout finished successfully')
