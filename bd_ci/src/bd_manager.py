d3c19569d5708cfc99eb2f701004a9515d59a181
WeiserAriel <41187619+WeiserAriel@users.noreply.github.com>
2020-03-23 10:30:50 +0200

    Update bd_manager.py

--- bd_ci/src/bd_manager.py
+++ bd_ci/src/bd_manager.py
@@ -93,15 +93,16 @@ def clone_repo(project, url):
                 sys.exit(1)                                                                 
         elif project == 'UFMAPL':                                                           
             print('cloning UFMAPL repository')                                              
-            os.system(cmd)                                                                  
+            os.system(cmd)
+            print('running git checkout for master')
+            os.system('git checkout ufmapl_4_2_0_ufm_6_3_0')
+            print('git checkout finished successfully')
             print('deleting irrelevent directories from UFMAPL project')                    
             for directory in('common_mlnx','demo','generic','switchx'):                     
                 dir_ = os.path.abspath(os.getcwd()) + '/tree/customer/' + directory          
                 print('removing directory : ' + str(dir_))                                  
                 shutil.rmtree(dir_)  
-            print('running git checkout for master')
-            os.system('git checkout ufmapl_4_2_0_ufm_6_3_0')
-            print('git checkout finished successfully')
+            
         else:                                                                               
             os.system(cmd)  # Cloning                                                       
     except Exception as e:                                                                  
